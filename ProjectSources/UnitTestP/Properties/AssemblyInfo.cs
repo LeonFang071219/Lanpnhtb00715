e93a4aa7f2fdebfc6ea168540aec1a8e18c1d0eb
LeonFang071219 <lanpnhtb00715@fpt.edu.vn>
2025-02-25 22:25:34 +0700

    Up file

---
+++ ProjectSources/UnitTestP/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestP")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3b6e0a44-fb86-4186-8c23-079b181feb64")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProjectSources/UnitTestP/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DAL_QLBanHang;
+using DTO_QLBanHang;
+
+namespace UnitTestP
+{
+    [TestClass]
+    public class UnitTest_QLBanHang : DBConnect
+    {
+        [TestMethod]
+        public void NhanVienLogin_TC01()
+        {
+            DTO_NhanVien nv = new DTO_NhanVien();
+            DAL_NhanVien login = new DAL_NhanVien();
+            nv.EmailNV = "fpoly@fe.edu.vn";
+            nv.MatKhau = "ABC";
+            bool result = login.NhanVienDangNhap(nv);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void NhanVienLogin_TC02()
+        {
+            DTO_NhanVien nv = new DTO_NhanVien();
+            DAL_NhanVien login = new DAL_NhanVien();
+            nv.EmailNV = "fpoly@fe.edu.vn";
+            nv.MatKhau = "23315424196402035621";
+            bool result = login.NhanVienDangNhap(nv);
+            Assert.IsTrue(result);
+        }
+    }
+}
